8a68fd4fa2f56fcac4b73ccd2df501237706a421
Hin1209 <hyeongin2024@gmail.com>
2023-05-12 11:25:22 +0000

    Feat: supplemental page table kill 구현

--- vm/vm.c
+++ vm/vm.c
@@ -302,4 +302,13 @@ void supplemental_page_table_kill(struct supplemental_page_table *spt UNUSED)
 {
 	/* TODO: Destroy all the supplemental_page_table hold by thread and
 	 * TODO: writeback all the modified contents to the storage. */
+	struct hash_iterator i;
+	struct page *remove_page = NULL;
+	hash_first(&i, &spt->spt_hash);
+	while (hash_next(&i))
+	{
+		if (remove_page != NULL)
+			vm_dealloc_page(remove_page);
+		remove_page = hash_entry(hash_cur(&i), struct page, page_elem);
+	}
 }
