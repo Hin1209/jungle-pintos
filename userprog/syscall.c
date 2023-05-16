09bfe66d108bde079eaf7b854c291552c7803d80
Ubuntu <ubuntu@ip-172-31-32-179.ap-northeast-2.compute.internal>
2023-05-16 11:25:37 +0000

    [delete comment]

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -64,8 +64,7 @@ syscall_init (void) {
 /* The main system call interface */
 void
 syscall_handler (struct intr_frame *f UNUSED) {
-	// TODO: Your implementation goes here.
-	uint32_t *sp = f -> rsp; /* 유저 스택 포인터 */
+	uint32_t *sp = f -> rsp; 
 	thread_current()->user_rsp = f->rsp;
 	check_address((void *)sp);
 
@@ -75,66 +74,49 @@ syscall_handler (struct intr_frame *f UNUSED) {
 	switch (a)
 	{
 	case SYS_HALT:
-		//printf("halt!\n");
 		halt();
 		break;
 	case SYS_EXIT:
-		// printf("exit!\n");
 		exit(f->R.rdi);
 		break;
 	case SYS_FORK:
-		// printf("fork!\n");
 		f->R.rax = fork(f->R.rdi, f);
 		break;
 	case SYS_EXEC:
-		// printf("exec!\n");
 		if (exec(f->R.rdi) == -1)
 			exit(-1);
 		break;
 	case SYS_WAIT:
-		// printf("wait!\n");
 		f->R.rax = process_wait(f->R.rdi);
 		break;
 	case SYS_CREATE:
-		// printf("create!\n");
 		f->R.rax = create(f->R.rdi, f->R.rsi);
 		break;
 	case SYS_REMOVE:
-		// printf("remove!\n");
 		f->R.rax = remove(f->R.rdi);
 		break;
 	case SYS_OPEN:
-		// printf("open!\n");
 		f->R.rax = open(f->R.rdi);
 		break;
 	case SYS_FILESIZE:
-		// printf("filesize!\n");
 		f->R.rax = filesize(f->R.rdi);
 		break;
 	case SYS_READ:
-		// printf("read!\n");
 		f->R.rax = read(f->R.rdi, f->R.rsi, f->R.rdx);
 		break;
 	case SYS_WRITE:
-		//printf("write!\n");
 		f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
 		break;
 	case SYS_SEEK:
-		//printf("seek!\n");
 		seek(f->R.rdi, f->R.rsi);
 		break;
 	case SYS_TELL:
-		//printf("tell!\n");
 		f->R.rax = tell(f->R.rdi);
 		break;
 	case SYS_CLOSE:
-		//printf("close!\n");
 		close(f->R.rdi);
 		break;
-	// case SYS_DUP2:
-	// 	printf("dup2!\n");
-	// 	// f->R.rax = dup2(f->R.rdi, f->R.rsi);
-	// 	break;
+
 	case SYS_MMAP:
 		f->R.rax = mmap(f->R.rdi, f->R.rsi, f->R.rdx, f->R.r10, f->R.r8);
 		break;
@@ -142,7 +124,6 @@ syscall_handler (struct intr_frame *f UNUSED) {
 		munmap(f->R.rdi);
 		break;
 	default:
-		// printf("default exit!\n");
 		exit(-1);
 		break;
 	}
@@ -153,31 +134,25 @@ void check_address(void *addr)
 	struct thread *curr = thread_current();
 	if (addr == NULL || !(is_user_vaddr(addr)))
 	{
-		// 주소가 null이 아니고, 커널 스택의 주소 아니고, 해당 가상 주소에 대한 PTE가 존재할 때만 시스템 콜을 호출할 자격이 있는 포인터다.
 		exit(-1);
 	}
-	/* 포인터가 가리키는 주소가 유저영역의 주소인지 확인 */
-	/* 잘못된 접근(유저 영역을 벗어난 영역)일 경우 프로세스 종료(exit(-1)) */
 }
 
 void halt(void)
 {
 	power_off();
-	//pintos 종료시키는 시스템 콜
-	/* power_off()를 사용하여 pintos 종료 */
 }
 
 void exit(int status)
 {
 	struct thread *cur = thread_current (); 
-			/* 프로세스 디스크립터에 exit status 저장 */
 	cur->exit_status = status;
 	printf("%s: exit(%d)\n" , cur->name , status);
 	thread_exit();
 }
 
 
-int exec(const *file) // cmd_line: 새로운 프로세스에 실행할 프로그램 명령어
+int exec(const *file) 
 {
 	check_address(file);
 	int file_size = strlen(file) + 1;
@@ -186,7 +161,7 @@ int exec(const *file) // cmd_line: 새로운 프로세스에 실행할 프로그
 	{
 			exit(-1);
 	}
-	strlcpy(fn_copy, file, file_size); // file 이름만 복사
+	strlcpy(fn_copy, file, file_size);
 	if (process_exec(fn_copy) == -1)
 	{
 			exit(-1);
@@ -200,61 +175,44 @@ tid_t fork(const char *thread_name, struct intr_frame *f) {
 
 int wait (tid_t pid)
 {
-	/* 자식 프로세스가 종료 될 때까지 대기 */
 	return process_wait(pid);
 }
 
 bool create (const char *file , unsigned initial_size)
 {
-	/*
-	- 파일 생성하는 시스템 콜
-	- 성공일 경우 true, 실패일 경우 false 리턴
-	- file: 생성할 파일의 이름 및 경로 정보
-	- initial_size: 생성할 파일 크기
-	*/
 	check_address(file);
 	return filesys_create(file, initial_size);
 }
 
 bool remove (const char *file)
 {
-	/*	
-  - 파일을 삭제하는 시스템 콜
-	- file : 제거할 파일의 이름 및 경로 정보
-	- 성공 일 경우 true, 실패 일 경우 false 리턴
-	*/
+	bool return_code;
 	check_address(file);
-	return filesys_remove(file);
+	lock_acquire(&filesys_lock);
+	return_code = filesys_remove(file);
+	lock_release(&filesys_lock);
+	return return_code;
 }
 
-/*
-* 파일을 열고 해당 파일 객체에 파일 디스크립터 부여하고 파일 디스크립터 반환
-*/
 int open (const char *file)
 {
 	check_address(file);
-	/* 파일을 open */
 	struct file *fileobj = filesys_open(file);
 
-	/* 해당 파일이 존재하지 않으면 -1 리턴 */
 	if (fileobj == NULL)
 	{
 		return -1;
 	}
-	/* 해당 파일 객체에 파일 디스크립터 부여 */ 
 	int fd = process_add_file(fileobj);
 
 	if (fd == -1)
 	{
 		file_close(fileobj);
 	}
-	/* 파일 디스크립터 리턴 */
+
 	return fd;
 }
 
-/*
-* fd에 해당하는 파일을 찾고 그 파일의 크기를 반환한다.
-*/
 int filesize(int fd)
 {
 	struct file *open_file = process_get_file(fd);
@@ -264,9 +222,6 @@ int filesize(int fd)
 	}
 	return file_length(open_file);
 }
-/*
-* fd를 이용해서 파일 객체를 검색하고 입력을 버퍼에 저장하고, 버퍼에 저장한 크기를 반환 
-*/
 int read(int fd, void *buffer, unsigned size)
 {
 	check_address(buffer);
@@ -282,8 +237,8 @@ int read(int fd, void *buffer, unsigned size)
 		char key;
 		for (read_byte = 0; read_byte < size; read_byte++)
 		{
-			key = input_getc(); // 키보드에 한 문자 입력받기
-			*read_buffer++ = key; // read_buffer에 받은 문자 저장
+			key = input_getc();
+			*read_buffer++ = key;
 			if (key == '\n')
 			{
 				break;
@@ -309,9 +264,7 @@ int read(int fd, void *buffer, unsigned size)
 	return read_byte;
 }
 
-/*
-* fd에 해당하는 파일을 열어서 buffer에 저장된 데이터를 size만큼 파일에 기록 후 기록한 바이트 수 리턴하는 함수
-*/
+
 int write(int fd, const void *buffer, unsigned size)
 {
 	check_address(buffer);
@@ -342,9 +295,7 @@ int write(int fd, const void *buffer, unsigned size)
 	return bytes_write;
 }
 
-/*
-* 파일에서 position으로 이동하는 함수
-*/
+
 void seek(int fd, unsigned position)
 {
 	struct file *seek_file = process_get_file(fd);
@@ -359,9 +310,6 @@ void seek(int fd, unsigned position)
 	file_seek(seek_file, position);
 }
 
-/*
-* 열린 파일의 위치를 반환하는 함수
-*/
 unsigned tell(int fd)
 {
 	struct file *tell_file = process_get_file(fd);
@@ -376,9 +324,6 @@ unsigned tell(int fd)
 	return file_tell(tell_file);
 }
 
-/*
-* 파일 디스크립터 엔트리 초기화
-*/
 void close (int fd)
 {
 	struct file *close_file = process_get_file(fd);
@@ -404,10 +349,6 @@ void *mmap(void *addr, size_t length, int writable, int fd, off_t offset)
 	if (file == NULL || length == 0)
 		return NULL;
 	return do_mmap(addr, length, writable, file, offset);
-	//fd로 열린 파일의 오프셋(offset) 바이트부터 length 바이트 만큼을 프로세스의 가상주소공간의 주소 addr 에 매핑 합니다.
-    //전체 파일은 addr에서 시작하는 연속 가상 페이지에 매핑됩니다. 파일 길이(length)가 PGSIZE의 배수가 아닌 경우 최종 매핑된 페이지의 일부 바이트가 파일 끝을 넘어 "stick out"됩니다. page_fault가 발생하면 이 바이트를 0으로 설정하고 페이지를 디스크에 다시 쓸 때 버립니다. 
-    //성공하면 이 함수는 파일이 매핑된 가상 주소를 반환합니다. 실패하면 파일을 매핑하는 데 유효한 주소가 아닌 NULL을 반환해야 합니다.
-	
 }
 
 void munmap(void *addr)
