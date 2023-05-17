89f8386a5aa30f7d698b65f0fc4fe6e94e5895cc
Hin1209 <hyeongin2024@gmail.com>
2023-05-17 06:55:27 +0000

    Feat: swap table 추가

--- include/vm/anon.h
+++ include/vm/anon.h
@@ -1,6 +1,10 @@
 #ifndef VM_ANON_H
 #define VM_ANON_H
 #include "vm/vm.h"
+#include "devices/disk.h"
+
+#define SLOT_SIZE 8
+
 struct page;
 enum vm_type;
 
@@ -8,6 +12,13 @@ struct anon_page
 {
     void *aux;
     // 디스크로 쫓겨난 위치 정보
+};
+
+struct swap_slot
+{
+    disk_sector_t start_sector;
+    struct list page_list;
+    struct list_elem slot_elem;
 };
 
 void vm_anon_init(void);
